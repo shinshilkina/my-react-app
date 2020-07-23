1ecbc1965480dc0dfccb7d6c5d019af876097f53
shinshilkina <anastasiashilina98@gmail.com>
2020-07-23 15:54:40 +0700

    video 07

--- src/App.js
+++ src/App.js
@@ -3,15 +3,32 @@ import './App.css';
 
 const App = () => {
   return (
-    <div className="App">
+    <div>
+        <Technologies/>
+        <Header/>
+    </div>
+  );
+}
+
+const Technologies = () => {
+    return (
         <ul>
             <li>css</li>
             <li>html</li>
             <li>js</li>
             <li>react</li>
         </ul>
+    );
+} 
+
+const Header = () => {
+  return (
+    <div className="Header">
+        This is <code>Header</code>! :)
     </div>
   );
 }
+
+
 
 export default App;
