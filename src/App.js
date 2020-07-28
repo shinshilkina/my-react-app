c276b0aafd353fd51464fedf065c38f8a0558b57
shinshilkina <anastasiashilina98@gmail.com>
2020-07-28 14:54:37 +0700

    добавила заглушку-страницу диалогов

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import {Header} from "./components/Header/Header";
 import {Navigation} from "./components/Navigation/Navigation";
 import {Profile} from "./components/Profile/Profile";
+import {Dialogs} from "./components/Dialogs/Dialogs";
 
 const App = () => {
   return (
     <div className='app-wrapper'>
         <Header />
         <Navigation />
-        <Profile />
+        {/*<Profile />*/}
+        <Dialogs />
     </div>
   );
 }
