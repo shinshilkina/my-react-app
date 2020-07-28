f8fbc7d060b822b6c26b52a2bc0b5afeb42c22df
shinshilkina <anastasiashilina98@gmail.com>
2020-07-28 14:52:08 +0700

    откорректировала код и расширила немножко функционал основным страниц

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,7 +4,7 @@ import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className={classes.header}>
+        <header className={`header ${classes.header}`}>
             <img className={classes.logo} src='https://images.clipartlogo.com/files/istock/previews/1072/107297409-vector-cute-yellow-chick-easy-logo.jpg' />
         </header>
     );
--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -7,8 +7,8 @@ let active = classes.item + ' ' + classes.active;
 const Navigation = () => {
     return (
         <nav className='navigation'>
-            <a href='#'> <div className={active}>Profile</div></a>
-            <a href='#'> <div className={classes.item}>Messages</div></a>
+            <a href='#'><div className={active}>Profile</div></a>
+            <a href='#'><div className={classes.item}>Messages</div></a>
             <a href='#'><div className={classes.item}>News</div></a>
             <a href='#'><div className={classes.item}>Music</div></a>
             <a href='#'><div className={classes.item}>Settings</div></a>
